9ab686b2927bd73032096511d05225af10ec2d3c
Victor <dir.victor@yandex.ru>
2024-05-09 20:44:10 +0300

    Добавление тестов и редактирование python-app.yml

---
+++ test_main.py
@@ -0,0 +1,16 @@
+import pytest
+from main import words_to_digits, file_reader
+
+def test1():
+    # Тест на обработку пустой строки
+    assert words_to_digits("") == ""
+
+def test2():
+    # Тест на обработку строки
+    assert words_to_digits("триста двадцать пять тысяч сто девять птиц съели семнадцать тысяч двести шесть яблок") == "325 109 птиц съели 17 206 яблок"
+
+def test3():
+    # Тест на обработку файла
+    assert file_reader("input.txt") == "325 109 птиц съели 17 206 яблок"
+
+pytest.main()
